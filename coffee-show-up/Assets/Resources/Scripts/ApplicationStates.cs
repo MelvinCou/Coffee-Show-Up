c31a265df419c508ba179c8d180139ca0cfda966
Melodie <melodie.lawson@epitech.eu>
2024-11-22 11:35:37 +0100

    feat: add scan-frame at app start
    Add an scan-frame to indicate to scan the marker. Add an ApplicationState Enum with an ApplicationManagement class for the global app behaviour

---
+++ coffee-show-up/Assets/Resources/Scripts/ApplicationStates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Resources.Scripts
+{
+    public enum ApplicationStates
+    {
+        APP_STARTED,
+        MARKER_SCANNED,
+        MODEL_SPAWNED
+    }
+}
