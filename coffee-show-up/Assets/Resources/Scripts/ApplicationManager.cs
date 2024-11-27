dc6b3745647b28047f50704252dd6d4b04a0b787
Melodie <melodie.lawson@epitech.eu>
2024-11-27 21:02:08 +0100

    feat: add indicative message for user
    add an introduction message to indicate to the user to scan the marker

--- coffee-show-up/Assets/Resources/Scripts/ApplicationManager.cs
+++ coffee-show-up/Assets/Resources/Scripts/ApplicationManager.cs
@@ -1,15 +1,25 @@
-using UnityEngine;
 using Assets.Resources.Scripts;
+using DG.Tweening;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class ApplicationManager : MonoBehaviour
 {
     public GameObject StartElementUI;
     public ApplicationStates AppState { get; set; }
 
+    private Text _presentationText;
 
     void Start()
     {
         AppState = ApplicationStates.APP_STARTED;
+        Text[] children = StartElementUI.GetComponentsInChildren<Text>();
+        _presentationText = children.First(text => text.name == "PresentationText");
+        if (_presentationText)
+        {
+            _presentationText.DOText("Scannez le marqueur de votre machine à café...", 5);
+        }
     }
 
     void Update()
