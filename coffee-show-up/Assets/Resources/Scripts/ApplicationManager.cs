accdcfb2571b973772b47557450bcd22e949c61b
Melodie <melodie.lawson@epitech.eu>
2025-01-10 09:15:01 +0100

    feat: add coffee machine big elements
    add the big elements of the nespresso machine, manage the different views

--- coffee-show-up/Assets/Resources/Scripts/ApplicationManager.cs
+++ coffee-show-up/Assets/Resources/Scripts/ApplicationManager.cs
@@ -7,17 +7,27 @@ using UnityEngine.UI;
 public class ApplicationManager : MonoBehaviour
 {
     public GameObject StartElementUI;
+
+    public GameObject InteractiveButtons;
+
+    public GameObject InnerParts;
     public ApplicationStates AppState { get; set; }
 
     public Button AudioButton;
+
+    public Button ChangeViewButton;
 
     public AudioSource AudioSource;
 
     private Text _presentationText;
 
+    private bool _innerView;
+
     void Start()
     {
         AppState = ApplicationStates.APP_STARTED;
+        InnerParts.SetActive(false);
+        InteractiveButtons.SetActive(false);
         Text[] children = StartElementUI.GetComponentsInChildren<Text>();
         _presentationText = children.First(text => text.name == "PresentationText");
         if (_presentationText)
@@ -26,6 +36,7 @@ public class ApplicationManager : MonoBehaviour
         }
 
         AudioButton.onClick.AddListener(PlayAudio);
+        ChangeViewButton.onClick.AddListener(ChangeView);
     }
 
     void Update()
@@ -34,10 +45,44 @@ public class ApplicationManager : MonoBehaviour
         {
             StartElementUI.SetActive(false);
         }
+
+        if(!AppState.Equals(ApplicationStates.MODEL_SPAWNED))
+        {
+            InnerParts.SetActive(false);
+            InteractiveButtons.SetActive(false);
+        } 
+        else
+        {
+            InteractiveButtons.SetActive(true);
+        }
     }
 
     void PlayAudio()
     {
         AudioSource.Play();
+    }
+
+    void ChangeView()
+    {
+        _innerView = !_innerView;
+
+        GameObject lateralPlane = GameObject.FindGameObjectWithTag("Right-Lateral-Plane");
+        GameObject upperPart = GameObject.FindGameObjectWithTag("Upper-Part");
+        if (lateralPlane != null && upperPart != null)
+        {
+            if (_innerView && AppState.Equals(ApplicationStates.MODEL_SPAWNED))
+            {
+                InnerParts.SetActive(true);
+                lateralPlane.transform.DOMoveY(0.05f, 5);
+                upperPart.transform.DOMoveY(0.05f, 5);
+            }
+            else
+            {
+                //lateralPlane.transform.DOMoveY(-0.05f, 5);
+                //upperPart.transform.DOMoveY(-0.05f, 5);
+                //DOTween.RewindAll();
+                InnerParts.SetActive(false);
+            }     
+        }
     }
 }
