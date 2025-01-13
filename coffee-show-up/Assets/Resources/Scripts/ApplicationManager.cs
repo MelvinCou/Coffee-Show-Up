b720503592eeca162e6522c67bca4b9766962fbf
Melodie <melodie.lawson@epitech.eu>
2025-01-13 22:18:35 +0100

    feat: add the exploded view
    add the exploded for the big elements, also add new state machine for managing the app

--- coffee-show-up/Assets/Resources/Scripts/ApplicationManager.cs
+++ coffee-show-up/Assets/Resources/Scripts/ApplicationManager.cs
@@ -11,21 +11,33 @@ public class ApplicationManager : MonoBehaviour
     public GameObject InteractiveButtons;
 
     public GameObject InnerParts;
+
     public ApplicationStates AppState { get; set; }
+
+    public CoffeeMachineStates CoffeeMachineState { get; set; }
+
+    public ModeStates Mode { get; set; }
 
     public Button AudioButton;
 
     public Button ChangeViewButton;
 
+    public Button ActiveExplodedViewButton;
+
     public AudioSource AudioSource;
 
     private Text _presentationText;
 
-    private bool _innerView;
+    private Sequence _innerPartsRevealSequence;
+
+    private Sequence _explodedViewSequence;
 
     void Start()
     {
         AppState = ApplicationStates.APP_STARTED;
+        CoffeeMachineState = CoffeeMachineStates.NO_SHOW;
+        Mode = ModeStates.NO_ACTIVE_MODE;
+       
         InnerParts.SetActive(false);
         InteractiveButtons.SetActive(false);
         Text[] children = StartElementUI.GetComponentsInChildren<Text>();
@@ -36,7 +48,8 @@ public class ApplicationManager : MonoBehaviour
         }
 
         AudioButton.onClick.AddListener(PlayAudio);
-        ChangeViewButton.onClick.AddListener(ChangeView);
+        ChangeViewButton.onClick.AddListener(ChangeViewState);
+        ActiveExplodedViewButton.onClick.AddListener(ChangeInnerPartsState);
     }
 
     void Update()
@@ -50,10 +63,13 @@ public class ApplicationManager : MonoBehaviour
         {
             InnerParts.SetActive(false);
             InteractiveButtons.SetActive(false);
+            Mode = ModeStates.NO_ACTIVE_MODE;
+            CoffeeMachineState = CoffeeMachineStates.NO_SHOW;
         } 
         else
         {
             InteractiveButtons.SetActive(true);
+            InnerParts.SetActive(true);
         }
     }
 
@@ -62,27 +78,79 @@ public class ApplicationManager : MonoBehaviour
         AudioSource.Play();
     }
 
-    void ChangeView()
+    void ChangeViewState()
     {
-        _innerView = !_innerView;
+        if(CoffeeMachineState.Equals(CoffeeMachineStates.EXTERNAL_VIEW))
+        {
+            CoffeeMachineState = CoffeeMachineStates.INNER_VIEW;
+        }
+        else
+        {
+            CoffeeMachineState = CoffeeMachineStates.EXTERNAL_VIEW;
+        }
 
+        ManageExternalAnimation();
+    }
+
+    void ManageExternalAnimation()
+    {
         GameObject lateralPlane = GameObject.FindGameObjectWithTag("Right-Lateral-Plane");
         GameObject upperPart = GameObject.FindGameObjectWithTag("Upper-Part");
+
         if (lateralPlane != null && upperPart != null)
         {
-            if (_innerView && AppState.Equals(ApplicationStates.MODEL_SPAWNED))
+            if (CoffeeMachineState.Equals(CoffeeMachineStates.INNER_VIEW))
             {
                 InnerParts.SetActive(true);
-                lateralPlane.transform.DOMoveY(0.05f, 5);
-                upperPart.transform.DOMoveY(0.05f, 5);
+                _innerPartsRevealSequence = DOTween.Sequence();
+                _innerPartsRevealSequence
+                    .Append(lateralPlane.transform.DOMoveY(0.1f, 5))
+                    .Join(upperPart.transform.DOMoveY(0.1f, 5))
+                    .SetAutoKill(false);
             }
-            else
+            else if(CoffeeMachineState.Equals(CoffeeMachineStates.EXTERNAL_VIEW))
             {
-                //lateralPlane.transform.DOMoveY(-0.05f, 5);
-                //upperPart.transform.DOMoveY(-0.05f, 5);
-                //DOTween.RewindAll();
+                _innerPartsRevealSequence.SmoothRewind();
                 InnerParts.SetActive(false);
-            }     
+            }
+        }
+    }
+
+    void ChangeInnerPartsState()
+    {
+        if (CoffeeMachineState.Equals(CoffeeMachineStates.INNER_VIEW))
+        {
+            CoffeeMachineState = CoffeeMachineStates.EXPLODED_VIEW;
+        }
+        else if (CoffeeMachineState.Equals(CoffeeMachineStates.EXPLODED_VIEW))
+        {
+            CoffeeMachineState = CoffeeMachineStates.INNER_VIEW;
+        }
+        ManageInnerPartsAnimation();
+    }
+
+    void ManageInnerPartsAnimation()
+    {
+        GameObject nozzle = GameObject.FindGameObjectWithTag("Nozzle");
+        GameObject piston = GameObject.FindGameObjectWithTag("Piston");
+        GameObject pump = GameObject.FindGameObjectWithTag("Pump");
+        GameObject pipes = GameObject.FindGameObjectWithTag("Pipes");
+        if (nozzle != null && piston != null && pump != null && pipes != null)
+        {
+            if (CoffeeMachineState.Equals(CoffeeMachineStates.EXPLODED_VIEW))
+            {
+                _explodedViewSequence = DOTween.Sequence();
+                _explodedViewSequence
+                    .Append(nozzle.transform.DOMove(new Vector3(0, 0.05f, 0), 5))
+                    .Join(piston.transform.DOMove(new Vector3(0, 0.05f, 0), 5))
+                    .Join(pump.transform.DOMove(new Vector3(0, 0.05f, 0), 5))
+                    .Join(pipes.transform.DOMove(new Vector3(0, 0.05f, 0), 5))
+                    .SetAutoKill(false);
+            }
+            else if(CoffeeMachineState.Equals(CoffeeMachineStates.INNER_VIEW))
+            {
+                _explodedViewSequence.SmoothRewind();
+            }
         }
     }
 }
