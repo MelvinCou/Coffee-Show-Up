9d3a731f98a123733713096642bb0dfb82a7e407
MelvinCou <melvin.courjaud@epitech.eu>
2025-01-16 13:45:53 +0100

    chore: fix incorrect characters in text `Scannez votre machine à café`

--- coffee-show-up/Assets/Resources/Scripts/ApplicationManager.cs
+++ coffee-show-up/Assets/Resources/Scripts/ApplicationManager.cs
@@ -44,7 +44,7 @@ public class ApplicationManager : MonoBehaviour
         _presentationText = children.First(text => text.name == "PresentationText");
         if (_presentationText)
         {
-            _presentationText.DOText("Scannez votre machine à café...", 5);
+            _presentationText.DOText("Scannez votre machine Ã  cafÃ©...", 5);
         }
 
         AudioButton.onClick.AddListener(PlayAudio);
